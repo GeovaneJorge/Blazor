a91d13257394129cb43315a45ff707a5d489e84a
Geovane Jorge <geovane.jorge@gmail.com>
2022-08-17 22:38:01 -0300

    Dtos criados

---
+++ BlazorBitconApp/DTOs/BitcoinDataDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBitconApp.DTOs
+{
+	public class BitcoinDataDTO
+	{
+	}
+}
+
+
+/* https://www.youtube.com/watch?v=fghFOg18LM4   3:22 minutos */
